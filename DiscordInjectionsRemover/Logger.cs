6c142ea17c8ab91707373c1d5277adf77a131bf6
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-10-24 22:09:03 +0000

    Add files via upload

---
+++ DiscordInjectionsRemover/Logger.cs
@@ -0,0 +1,21 @@
+﻿using Crayon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordInjectionsRemover
+{
+    public static class Logger
+    {
+        public static void Info(string x)
+        {
+            Console.WriteLine($"{Output.Yellow("[")}{Output.Blue("INFO")}{Output.Yellow("]")}: {x}");
+        }
+        public static void Warn(string x)
+        {
+            Console.WriteLine($"{Output.Yellow("[")}{Output.Red("WARN")}{Output.Yellow("]")}: {x}");
+        }
+    }
+}
---
+++ DiscordInjectionsRemover/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace DiscordInjectionsRemover
+{
+    class Program
+    {
+        public static string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), assemblyVersion = Assembly.GetEntryAssembly().GetName().Version.ToString(), version = assemblyVersion.Remove(assemblyVersion.Length-4);
+        public static List<string> paths = new List<string>()
+        {
+            $"{localAppData}\\Discord",
+            $"{localAppData}\\DiscordCanary",
+            $"{localAppData}\\DiscordDevelopment",
+            $"{localAppData}\\DiscordPTB"
+        };
+        static void Main(string[] args)
+        {
+            Console.Title = $"Injections Remover - {version}";
+            foreach(var x in paths)
+            {
+                if (Directory.Exists(x))
+                {
+                    Logger.Info($"Verifying {x.Split('\\')[x.Split('\\').Length-1]}");
+                    try
+                    {
+                        string folder = $"{Directory.GetDirectories($"{Directory.GetDirectories(x)[0]}\\modules")[1]}\\discord_desktop_core";
+                        string index = $"{folder}\\index.js";
+                        if (File.Exists(index))
+                        {
+                            if (File.ReadAllText(index) == "module.exports = require('./core.asar');")
+                                Logger.Info("index.js | Not infected");
+                            else
+                            {
+                                File.WriteAllText(index, "module.exports = require('./core.asar');");
+                                Logger.Warn("index.js | Potentially Infected ! Fixed the file !");
+                            }
+                            foreach(var lmao in Directory.GetDirectories(folder))
+                            {
+                                Logger.Warn($"Found a directory \"{lmao.Split('\\')[lmao.Split('\\').Length - 1]}\", Fixed it.");
+                                Directory.Delete(lmao, true);
+                            }
+                        }
+                    } catch
+                    {
+                        Logger.Info("Hum.. Looks like a not completelly Installed Discord Client or badly removed.. I'll fix it by deleting it.");
+                        Directory.Delete(x, true);
+                    }
+                }
+            }
+            Logger.Info("Finished.");
+            Console.ReadLine();
+        }
+    }
+}
